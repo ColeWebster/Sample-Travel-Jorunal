36fffd545a46a4332b26990ae3cbb0359491a178
Cole Webster <CWebster_Coding@outlook.com>
2022-06-20 15:34:48 -0400

    Added icons, text and styling to navbar

--- travel-journal/src/components/Header.js
+++ travel-journal/src/components/Header.js
@@ -1,5 +1,11 @@
+import React from "react";
+import { FaItunes } from "react-icons/fa";
+
 export default function Header() {
-    return (
-        <h1>This is working</h1>
-    )
+  return (
+    <div className="navbar">
+      <FaItunes size={70} alt="logo" className="navbar_logo" />
+      <h1 className="navbar_header">My Concert Journal</h1>
+    </div>
+  );
 }
