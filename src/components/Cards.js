139415e772a5906b4a152fa270a812d9623420d4
Cole Webster <CWebster_Coding@outlook.com>
2022-06-21 15:03:06 -0400

    Updated css and card to map over data

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,33 +1,32 @@
 import Data from "../Data";
-import Bleachers from "../assets/bleachers.jpg";
 import { FaMapPin } from "react-icons/fa";
 
 export default function Cards() {
-  return (
-    <section className="card_container">
-      
-      <img src={Bleachers} className="card_img" />
-      
-      <div className="card_info">
-        <div className="card_info_header">
-          <FaMapPin size={20} className="card_pin_logo" alt="pin" />
-          <p className="card_location">St. Pete</p>
-          <a 
-            href='https://goo.gl/maps/eBLYyWT1rB6Bmmyk6' target="_blank"
-            rel="nofollow"
-          >
-            View on Google Maps
-          </a>
-        </div>
+  const cards = Data.map((card) => {
+    return (
+      <section className="card_container">
+        <img src={card.image} className="card_img" />
 
-        <h1 className="card_bandName">Bleachers</h1>
-        <p className="card_date">Date</p>
-        <p className="card_text">
-          Saw Bleachers on their stop in Florida after canceling the first tour
-          due to concerns over the COVID-19 virus. Song highlights included:
-          'Rollercoaster' 'Don't Take the Money' 'I Wanna Get Better'.
-        </p>
-      </div>
-    </section>
-  );
+        <div className="card_info">
+          <div className="card_info_header">
+            <FaMapPin size={20} className="card_pin_logo" alt="pin" />
+            <p className="card_location">{card.location}</p>
+            <a href={card.mapsLink} target="_blank" rel="nofollow">
+              View on Google Maps
+            </a>
+          </div>
+
+          <h1 className="card_bandName">{card.title}</h1>
+          <p className="card_date">{card.date}</p>
+          <p className="card_text">{card.description}</p>
+        </div>
+      </section>
+    );
+  });
+
+  return (
+    <>
+      {cards}
+    </>
+  )
 }
